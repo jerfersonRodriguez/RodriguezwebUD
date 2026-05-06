823aa9ddbea354b32bf4b9d0577664837d762ae2
jerfer <jerfer@DESKTOP-PEUCOKO.lan>
2026-05-06 16:48:13 -0500

    trabajo de prime

---
+++ RodriguezwebUD-master/rodrigueWebUD/src/main/java/asp/modelo/AspiranteDAO.java
@@ -0,0 +1,13 @@
+package asp.modelo;
+
+import java.util.ArrayList;
+
+public class AspiranteDAO {
+
+	public static ArrayList<Aspirante> lista_A = new ArrayList<Aspirante>();
+	
+	
+}
+
+
+//Autor: Jerferson David Rodriguez Guarnizo
